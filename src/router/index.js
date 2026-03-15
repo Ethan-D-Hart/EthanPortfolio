27fda49d3911284b347652dc9f86f9cf6ab9add5
Ethan Hart <kyedhart@gmail.com>
2026-03-14 19:56:33 -0500

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomeView },
-    { path: '/about', component: () => import('../views/AboutView.vue') },
-    { path: '/contact', component: () => import('../views/ContactView.vue') },
-    { path: '/faq', component: () => import('../views/FaqView.vue') },
-  ],
-})
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes: [
+//     { path: '/', component: HomeView },
+//     { path: '/about', component: () => import('../views/AboutView.vue') },
+//     { path: '/contact', component: () => import('../views/ContactView.vue') },
+//     { path: '/faq', component: () => import('../views/FaqView.vue') },
+//   ],
+// })
 
-export default router
+
+
+// export default router
+
+export default {
+  async fetch(request, env) {
+    // This tells the worker to look for the file in your 'dist' folder
+    return env.ASSETS.fetch(request);
+  },
+};
